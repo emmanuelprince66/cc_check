48ebf07b3b834f25ed09cba7c0552570efcff777
DisuAde <idrisadekunle726@gmail.com>
2023-10-01 23:28:45 +0100

    add ?

--- src/components/handlePlacingOrder/handlePlacingOrder.jsx
+++ src/components/handlePlacingOrder/handlePlacingOrder.jsx
@@ -1696,7 +1696,7 @@ export const PlaceOrder = ({ supermarketCart, restaurant }) => {
       </Modal>
       {/* Modal 4 ends*/}
       {/* Modal 5* success response for restaurant & supermarket*/}
-      {supermarketCart.length === 0 ? (
+      {supermarketCart?.length === 0 ? (
         <Modal
           className="scale-in-center"
           open={successResponse}
